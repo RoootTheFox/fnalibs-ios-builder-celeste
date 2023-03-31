25c26cba3fee86997e9bf429435dd81370c267da
rooot <rooot@foxes4life.net>
2023-03-31 18:40:26 +0200

    feat: updatelibs changes
    Signed-off-by: rooot <rooot@foxes4life.net>

--- updatelibs.sh
+++ updatelibs.sh
@@ -10,20 +10,23 @@
 if [ ! -d "./SDL2/" ]; then
 	echo "SDL2 folder not found. Cloning now..."
 	git clone https://github.com/libsdl-org/SDL.git SDL2
+	cd "./SDL2/"
+	git checkout 257cacab183b312bbe60bd7967eee44a3ad7be85
+	cd ..
 
 	echo ""
 fi
 
 if [ ! -d "./FNA3D/" ]; then
 	echo "FNA3D folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FNA3D.git --recursive
+	git clone https://github.com/FNA-XNA/FNA3D.git --recursive --branch 21.03 --single-branch
 
 	echo ""
 fi
 
 if [ ! -d "./FAudio/" ]; then
 	echo "FAudio folder not found. Cloning now..."
-	git clone https://github.com/FNA-XNA/FAudio.git
+	git clone https://github.com/FNA-XNA/FAudio.git --branch 21.03.05
 
 	echo ""
 fi
@@ -31,13 +34,16 @@ fi
 if [ ! -d "./Theorafile/" ]; then
 	echo "Theorafile folder not found. Cloning now..."
 	git clone https://github.com/FNA-XNA/Theorafile
+	cd "./Theorafile/"
+	git checkout 0c5504658a3108919e53b625287786a87529de42
+	cd ..
 
 	echo ""
 fi
 
 if [ ! -d "./MoltenVK" ]; then
 	echo "MoltenVK folder not found. Cloning now..."
-	git clone --recursive https://github.com/KhronosGroup/MoltenVK
+	git clone --recursive https://github.com/KhronosGroup/MoltenVK --branch v1.1.4 --single-branch
 fi
 
 # Check for updates...
